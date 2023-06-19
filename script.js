2c2e2bbedf0a3fc1b8216f2b6c2eb2a513f3edad
Chynara <62700904+samarbekova-source@users.noreply.github.com>
2023-06-19 14:28:56 +0400

    last commit

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ let image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randdomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🎆Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "🎆Player 2 Wins!";
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
 } else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
